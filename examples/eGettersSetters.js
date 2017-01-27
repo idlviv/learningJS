2a84c6886750b9c84eb759cadbdae251d023502b
idlviv <idlviv@rambler.ru>
2017-01-27 14:00:24 +0200

    27/01/17 work

--- examples/eGettersSetters.js
+++ examples/eGettersSetters.js
@@ -1,11 +1,54 @@
+'use strict';
 var person = {
-  name : 'John',
-  _age : 21,
-  set age (value){
-    this._age = value < 0 ? 0 : value >122 ? 122: value;
+  'name': 'John',
+  '_age': 21,
+  'method': function meth() {
+    console.log('myMethod');
   },
-  get age (){
+  get 'age'() {
     return this._age;
+  },
+  set 'age'(value) {
+    this._age = value < 0 ? 0 : value > 122 ? 122 : value;
   }
 };
-console.log (person.age);
+// console.log(person.age);
+// console.log(Object.getOwnPropertyDescriptor(person, 'name'));
+// console.log(Object.getOwnPropertyDescriptor(person, 'method'));
+// console.log(Object.getOwnPropertyDescriptor(person, 'age'));
+
+Object.defineProperty(person, 'gender', {
+  'value': 'male',
+  'writable': false,
+  'enumerable': false,
+  'configurable': false
+});
+console.log(Object.getOwnPropertyDescriptor(person, 'gender'));
+
+// not allowed with 'use strict'
+// for (property in person) {
+//   console.log(property);
+// }
+
+console.log(Object.keys(person));
+console.log(person.propertyIsEnumerable('gender'));
+
+var obj = {};
+Object.defineProperties(obj, {
+  'size': {
+    'value': '10',
+    'writable': false,
+    'enumerable': false,
+    'configurable': false
+  }
+});
+console.log(obj.size);
+
+Object.preventExtensions(obj);
+console.log(Object.isExtensible(obj));
+
+Object.seal(obj);
+console.log(Object.isSealed(obj));
+
+Object.freeze(obj);
+console.log(Object.isFrozen(obj));
--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-angular.module('id',[]);
-
-angular.module('id').controller('myController', function ($scope) {
-  $scope.user = 'John';
-});
+'use strict';
+function getCount(str) {
+  var vowelsCount = 0;
+  var ch = str.split('');
+  console.log(ch);
+  for (var i = 0; i < ch.length; i++) {
+    // if (ch[i] === 'a' || ch[i] === 'e' || ch[i] === 'i' ||
+    //   ch[i] === 'o' || ch[i] === 'u') {
+    if (ch[1]){
+    vowelsCount += vowelsCount;
+    }
+  }
+  console.log(vowelsCount);
+  console.log(ch[0]);
+  return vowelsCount;
+}
+getCount('hello');
