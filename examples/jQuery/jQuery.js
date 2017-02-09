38235ebb8a51e5f3fd8932a54205f92ee15291e5
idlviv <idlviv@rambler.ru>
2017-02-09 20:27:00 +0200

    09/02/17 home

---
+++ examples/jQuery/jQuery.js
@@ -0,0 +1,65 @@
+var parentsU;
+$(document).ready(function() {
+
+// $('.texts').append('<button>autoBtn</button>');
+}
+// $('<div>!!!!!!!!!!!!!</div>').appendTo('#eventLog');
+
+// $(window).resize(function(e) {
+//   $('h1').text('width' + window.innerWidth);
+// });
+// $('#btnOk').hover(function(e) {
+//   // console.log(e);
+//   e.target.textContent = 'Pressed';
+//   //$(this).text('Pressed');
+// });
+//
+// $('#btnTest1').on({
+//     click: function(e) {
+//       e.target.textContent = 'click';
+//     },
+//
+//     mouseover: function(e) {
+//       console.log('press');
+//       e.target.textContent = 'mouse over ' + e.data.user;
+//     }
+//   },
+//   {user: 'userText'}
+// );
+
+// var greeting = $('#greeting');
+// console.log(greeting.parents().length);
+// console.log(greeting);
+// console.log(greeting.parentsUntil('article'));
+//
+// var menuItem = $('#list3');
+// var miA = menuItem.nextAll();
+// var miU = menuItem.nextUntil('#list7');
+// console.log(miU[0], typeof(miU));
+// console.log(miU[1], typeof(miU));
+// console.log(miU[2], typeof(miU));
+// console.log(miU.length, typeof(miU));
+// console.log(miU[4], typeof(miU));
+//
+// var article = $('article');
+// console.log(article.children());
+// console.log(article.contents());
+//
+// var sibl = $('li').first().siblings();
+// for (var i = 0; i < sibl.length; i++){
+//   console.log(sibl[i]);
+// }
+
+// console.log('li.list', $('li.list'));
+// console.log('li, .list', $('li, .list'));
+// console.log('div p', $('div p'));
+// console.log('div>p', $('div>p'));
+// console.log('i+b', $('i+b'));
+
+
+//
+// window.addEventListener('load', function () {
+//   parentsU.forEach(function (item) {
+//     console.log(item);
+// })});
+});
